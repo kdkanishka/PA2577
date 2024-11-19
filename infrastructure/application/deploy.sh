9d43d26ecadc334900a3f36d1e117e40ffb76e61
Kanishka Dilshan <kdkanishka@gmail.com>
2024-11-19 10:41:01 +0100

    updating the deployment script to deploy services prio to the pods

--- infrastructure/application/deploy.sh
+++ infrastructure/application/deploy.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# Deploy the mongoDB
 kubectl apply -f mongodb-configmap.yaml
 kubectl apply -f mongodb-pvc.yaml
-kubectl apply -f mongodb-deployment.yaml
 kubectl apply -f mongodb-service.yaml
-kubectl apply -f shoppinglist-service-deployment.yaml
+kubectl apply -f mongodb-deployment.yaml
+
+# Deploy the shopping list service component
 kubectl apply -f shoppinglist-service-service.yaml
-kubectl apply -f shoppinglist-api-deployment.yaml
+kubectl apply -f shoppinglist-service-deployment.yaml
+
+# Deploy the shopping list API component
 kubectl apply -f shoppinglist-api-service.yaml
+kubectl apply -f shoppinglist-api-deployment.yaml
